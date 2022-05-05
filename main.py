fe7af3bf56823864489c4a9b8e7ce62f996afada
Diaszano <lucasdiiassantos@gmail.com>
2022-05-05 07:36:55 -0300

    arrumando identação

--- main.py
+++ main.py
@@ -14,15 +14,14 @@ def rastrearEncomendaBanco(codigo:str = '',idUser:str = '',novo:bool = False)-> 
 
 def rastrearEncomendaNova(codigo:str = '',idUser:str = '') -> bool or list:
     if not(rastrearEncomendaBanco(codigo=codigo,idUser=idUser,novo=True)):
-        url = f'https://proxyapp.correios.com.br/v1/sro-rastro/{codigo}';
+        url         = f'https://proxyapp.correios.com.br/v1/sro-rastro/{codigo}';
         informacoes = requests.get(url);
         informacoes = str(informacoes.text);
-        print(informacoes);
         informacoes = re.findall('(?P<Eventos>\"eventos\"\:)(?P<Dados_Eventos>\[.*?\])', informacoes, re.MULTILINE | re.IGNORECASE);
         if informacoes != []:
-            informacoes    = str(informacoes);
-            informacoes    = re.findall('(?P<Eventos>\{\"codigo\"\:.*?\.png\"\})*', informacoes, re.MULTILINE | re.IGNORECASE);
-            resultados = [valor for valor in informacoes if valor != ''];
+            informacoes = str(informacoes);
+            informacoes = re.findall('(?P<Eventos>\{\"codigo\"\:.*?\.png\"\})*', informacoes, re.MULTILINE | re.IGNORECASE);
+            resultados  = [valor for valor in informacoes if valor != ''];
             return limparMensagem(eventos=resultados);
         else:
             return False;
@@ -74,15 +73,14 @@ def limparMensagem(eventos:list = []) -> list:
                 uf          = uf.replace('"','');
                 cidade      = cidade.replace('"','');
                 destino     = f' para [{cidade}/{uf}]';
-        rastreio += f'[{limpaData(dia)}] - {descricao} {local}{destino}\n{detalhe}\n\n'
-    print(rastreio);
+        rastreio += f'[{limpaData(dia)}] - {descricao} {local}{destino}\n{detalhe}\n\n';
     return rastreio;
 
 def limpaData(data:str='')->str:
-    ano = data[:4];
-    mes = data[5:7];
-    dia = data[8:10];
-    hora= data[11:];
+    ano      = data[:4];
+    mes      = data[5:7];
+    dia      = data[8:10];
+    hora     = data[11:];
     mensagem = f"{dia}/{mes}/{ano} - {hora}";
     return mensagem;
 
@@ -91,7 +89,7 @@ def verificacaoDeCpf(numerosDoCpf:str = '000.000.000-00')->bool:
     if len(cpf) != 11 or cpf == cpf[::-1]:
         return False;
     for i in range(9, 11):
-        valor = sum((cpf[num] * ((i + 1) - num) for num in range(0, i)));
+        valor  = sum((cpf[num] * ((i + 1) - num) for num in range(0, i)));
         digito = ((valor * 10) % 11) % 10;
         if digito != cpf[i]:
             return False;
@@ -99,8 +97,8 @@ def verificacaoDeCpf(numerosDoCpf:str = '000.000.000-00')->bool:
 
 def verificacaoDeCnpj(cnpj:str = '00.000.000/0000-00')->bool:
     cnpjo = [int(char) for char in cnpj if char.isdigit()];
-    cnpj = cnpjo[:12];
-    prod = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+    cnpj  = cnpjo[:12];
+    prod  = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
     
     while len(cnpj) < 14:
         valor = sum([x*y for (x, y) in zip(cnpj, prod)])%11;
@@ -121,8 +119,8 @@ if __name__ == '__main__':
 
     @bot.message_handler(commands=["rastrear"])
     def rastrear(mensagem):
-        codigo = mensagem.text.replace('/rastrear ','');
-        idUser = mensagem.chat.id;
+        codigo   = mensagem.text.replace('/rastrear ','');
+        idUser   = mensagem.chat.id;
         resposta = f"Procurando encomenda";
         bot.reply_to(mensagem,resposta);
         resposta = rastrearEncomendaNova(codigo=codigo,idUser=idUser);
@@ -134,7 +132,7 @@ if __name__ == '__main__':
     
     @bot.message_handler(commands=["encomendas"])
     def atualizarEncomendas(mensagem):
-        idUser = mensagem.chat.id;
+        idUser     = mensagem.chat.id;
         encomendas = rastrearEncomendaBanco(idUser=idUser);
         if encomendas == False:
             resposta = f"Infelizmente tu não contém nenhuma encomenda guardada";
@@ -176,6 +174,5 @@ if __name__ == '__main__':
         \t\tExemplo /rastrear QK395235673BR
         Responder qualquer outra coisa não vai funcionar, clique em uma das opções"""
         bot.reply_to(mensagem, texto);
-
     bot.polling();
 #-----------------------
