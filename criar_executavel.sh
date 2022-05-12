4e9e3b4cb4de59966c46525d8b5717d66ee09d61
Diaszano <lucasdiiassantos@gmail.com>
2022-05-12 00:13:08 -0300

    add criador de executável

---
+++ criar_executavel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pyinstaller --console --clean --distpath=linuxdist --workpath=temporario main.py --onefile --name bot_telegram.dzn
+rm -r *.spec
+rm -r ./__pycache__/
+cp ./linuxdist/* ./
+rm -r ./temporario/
+rm -r ./linuxdist/
+clear
+echo "Cridado o executavel"
